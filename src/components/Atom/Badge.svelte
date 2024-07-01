b81387180e4797d0f2dcecaa1810071c01b92932
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2024-07-01 19:59:27 -0400

    Update: Badge component to svelte

---
+++ src/components/Atom/Badge.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let text = ""
+	export let bgClass = ""
+</script>
+
+<span
+	class={`mb-2 mr-2 inline-block rounded-full px-3 py-2 text-sm font-semibold text-white ${bgClass}`}
+>
+	{text}
+</span>
