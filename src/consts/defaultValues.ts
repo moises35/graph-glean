468070c4e49bd97f80621e492218c4fe34b4d6fa
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2024-07-01 19:58:08 -0400

    Add: default values for OpenGraph metadata

---
+++ src/consts/defaultValues.ts
@@ -0,0 +1,8 @@
+export const DEFAULT__OPENGRAPH_VALUES = {
+	og_title: "No title",
+	og_url: "No URL",
+	og_description: "No description",
+	og_image: "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=",
+	twitter_title: "No twitter title",
+	twitter_description: "No twitter description",
+}
