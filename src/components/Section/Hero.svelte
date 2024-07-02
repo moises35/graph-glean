4794e14d43b45cd2e9843f07fa409f77f8184c84
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2024-07-01 20:00:13 -0400

    Add: Hero section with OpenGraph form and component

---
+++ src/components/Section/Hero.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import Form from "@/components/Atom/Form.svelte"
+	import OpenGraph from "@/components/Section/OpenGraph.svelte"
+	import type { OpenGraphData } from "@/types"
+
+	let dataOpenGraph: OpenGraphData = {}
+
+	const handleUpdateData = (event: CustomEvent<OpenGraphData>) => {
+		dataOpenGraph = event.detail
+	}
+</script>
+
+<div class="relative mb-8">
+	<div
+		aria-hidden="true"
+		class="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20"
+	>
+		<div
+			class="h-56 bg-gradient-to-br from-primary to-purple-400 blur-[106px] dark:from-blue-700"
+		>
+		</div>
+		<div class="h-32 bg-gradient-to-r from-cyan-400 to-sky-300 blur-[106px] dark:to-indigo-600">
+		</div>
+	</div>
+	<div class="mx-auto max-w-7xl px-6 md:px-12 xl:px-6">
+		<div class="relative ml-auto pt-20">
+			<div class="mx-auto text-center lg:w-2/3">
+				<div
+					class="gradient-text to-danger bg-gradient-to-r from-primary to-purple-400 bg-clip-text pb-4 text-center text-8xl font-extrabold tracking-tighter text-transparent"
+				>
+					GraphGlean
+				</div>
+				<p class="mt-5 text-gray-700 dark:text-gray-300">
+					Optimiza tus publicaciones en redes sociales con el Protocolo Open Graph.
+				</p>
+				<p class="mb-6 mt-1 text-gray-700 dark:text-gray-300">
+					Implementar etiquetas Open Graph asegura que tus enlaces se compartan con imágenes,
+					títulos y descripciones precisas, mejorando la visibilidad y el atractivo de tu
+					contenido en línea.
+				</p>
+				<Form on:update={handleUpdateData} />
+			</div>
+		</div>
+	</div>
+</div>
+
+<OpenGraph data={dataOpenGraph} />
+
+<style>
+
+</style>
---
+++ src/components/Section/OpenGraph.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Discord from "@/components/Atom/SocialNetwork/Discord.svelte"
+	import Facebook from "@/components/Atom/SocialNetwork/Facebook.svelte"
+	import Instagram from "@/components/Atom/SocialNetwork/Instagram.svelte"
+	import Linkedin from "@/components/Atom/SocialNetwork/Linkedin.svelte"
+	import Twitter from "@/components/Atom/SocialNetwork/Twitter.svelte"
+	import Whatsapp from "@/components/Atom/SocialNetwork/Whatsapp.svelte"
+	import type { OpenGraphData } from "@/types"
+
+	export let data: OpenGraphData = {}
+</script>
+
+<div>
+	{#if Object.keys(data).length > 0}
+		<div class="container mx-auto px-8 py-4 pb-10">
+			<!-- Social Network Container -->
+			<div
+				class="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0 lg:grid-cols-3"
+			>
+				<Facebook {data} />
+				<Whatsapp {data} />
+				<Instagram {data} />
+				<Discord {data} />
+				<Twitter {data} />
+				<Linkedin {data} />
+			</div>
+		</div>
+	{/if}
+</div>
