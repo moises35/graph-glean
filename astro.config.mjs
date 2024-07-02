486808b9c1e5ba736d361459a4465ac816bce6e2
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2024-07-02 12:21:41 -0400

    Update: meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
-import svelte from "@astrojs/svelte";
+import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()]
-});
+})
